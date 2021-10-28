8a1834c68e72679544217e88df1bd58bd22058e9
RagulKanna <ragulragi619@gmail.com>
2021-10-28 12:43:09 +0530

    Added:EmployeeWage Usecase 2- calculate daily wage

--- src/main/kotlin/EmployeeWage.kt
+++ src/main/kotlin/EmployeeWage.kt
@@ -1,14 +1,35 @@
 fun main(args: Array<String>) {
-    val isPresent = 1
-    val isAbsent = 0
-    if(rand(isAbsent, isPresent) == 0){
-        println("The Employee Is Absent")
+    val employee = Employee();
+    val isPresent: Int = 1
+    val isAbsent: Int = 0
+    val WAGE_PER_HOUR: Int = 20
+    val FULL_DAY_HOUR: Int = 8
+
+    println("The daily wage of the employee is ${employee.calculateDailyWage(WAGE_PER_HOUR, FULL_DAY_HOUR)}" )
+    employee.displayPresentOrAbsentWithDailyWage(isAbsent, isPresent, WAGE_PER_HOUR)
+
+}
+class Employee {
+    private fun checkPresentOrAbsent(isAbsent: Int, isPresent: Int): Int {
+        return (isAbsent..isPresent).random()
     }
-    else{
-        println("The Employee Is Present")
+    fun displayPresentOrAbsentWithDailyWage(isAbsent: Int, isPresent: Int, WAGE_PER_HOUR: Int) {
+        if(checkPresentOrAbsent(isAbsent, isPresent) == 0){
+            val FULL_DAY_HOUR: Int = 0
+            println("\nThe Employee Is Absent")
+            println("Today employee's wage is ${calculateDailyWage(WAGE_PER_HOUR, FULL_DAY_HOUR)}" )
+        }
+        else{
+            val FULL_DAY_HOUR: Int = 8
+            println("\nThe Employee Is Present")
+            println("Today employee's wage is ${calculateDailyWage(WAGE_PER_HOUR, FULL_DAY_HOUR)}" )
+        }
+    }
+
+    fun calculateDailyWage(wagePerHour: Int, fullDayHour: Int): Int {
+        val dailyWage = wagePerHour * fullDayHour
+        return (dailyWage)
     }
 }
 
-fun rand(isAbsent: Int, isPresent: Int): Int {
-    return (isAbsent..isPresent).random()
-}
+
